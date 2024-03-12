570a393f9839929f0d246074f093d7c6e8d49210
Nathan Johnson <nathan@dozen3.com>
2024-03-12 12:39:16 -0600

    temp

--- build/render.php
+++ build/render.php
@@ -1,6 +1,5 @@
 <?php namespace ttm\forms; ?>
 
 <div <?php echo get_block_wrapper_attributes(); ?>>
-	<p>Testing123</p>
 	<?php form_shortcode_callback(1); ?>
 </div>
--- src/edit.js
+++ src/edit.js
@@ -13,8 +13,6 @@ import { __ } from '@wordpress/i18n';
  */
 import {
     useBlockProps,
-    ColorPalette,
-    InspectorControls,
 } from '@wordpress/block-editor';
 import ServerSideRender from '@wordpress/server-side-render';
 
@@ -27,10 +25,6 @@ import ServerSideRender from '@wordpress/server-side-render';
  */
 import './editor.scss';
 
-import { useState } from 'react';
-
-import { PanelBody } from '@wordpress/components';
-
 /**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
@@ -39,67 +33,10 @@ import { PanelBody } from '@wordpress/components';
  *
  * @return {Element} Element to render.
  */
-export default function Edit( { attributes, setAttributes } ) {
-
-	const [ button__background_color, setActiveColor ] = useState();
+export default function Edit( { attributes } ) {
 
 	return (
 		<div { ...useBlockProps() }>
-			<InspectorControls key="setting">
-				<PanelBody
-        			title = {__( 'Colors', 't3-schema' ) }
-        			initialOpen = { true }
-   				 >
-					<fieldset>
-						<legend className="blocks-base-control__label">
-							{ __( 'Button Background Color', 't3-schema' ) }
-						</legend>
-						<ColorPalette
-							value = { attributes.button__background_color }
-							onChange = { ( color ) => {
-								setActiveColor( color )
-								setAttributes( { button__background_color:color } )
-							}}
-						/>
-					</fieldset>
-					<fieldset>
-						<legend className="blocks-base-control__label">
-							{ __( 'Active Button Background Color', 't3-schema' ) }
-						</legend>
-						<ColorPalette
-							value = { attributes.button__background_color__active }
-							onChange = { ( color ) => {
-								setActiveColor( color )
-								setAttributes( { button__background_color__active:color } )
-							}}
-						/>
-					</fieldset>
-					<fieldset>
-						<legend className="blocks-base-control__label">
-							{ __( 'Body Color', 't3-schema' ) }
-						</legend>
-						<ColorPalette
-							value = { attributes.body__color }
-							onChange = { ( color ) => {
-								setActiveColor( color )
-								setAttributes( { body__color:color } )
-							}}
-						/>
-					</fieldset>
-					<fieldset>
-						<legend className="blocks-base-control__label">
-							{ __( 'Body Background Color', 't3-schema' ) }
-						</legend>
-						<ColorPalette
-							value = { attributes.body__background_color }
-							onChange = { ( color ) => {
-								setActiveColor( color )
-								setAttributes( { body__background_color:color } )
-							}}
-						/>
-					</fieldset>
-				</PanelBody>
-			</InspectorControls>
 			<ServerSideRender
 				block="ttm/form"
 				attributes={ attributes }
