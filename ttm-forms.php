106d158850f9a958c80e4ecdb9ac77028cc81a57
Nathan Johnson <nathan@dozen3.com>
2024-03-18 12:34:08 -0600

    Add ttm forms entries table

--- ttm-forms.php
+++ ttm-forms.php
@@ -13,6 +13,9 @@
 
 namespace ttm\forms;
 
+global $wpdb;
+define( 'TTM_FORMS_TABLE_NAME', $wpdb->prefix . 'ttm_forms' );
+
 /**
  * 
  */
@@ -193,7 +196,7 @@ function process_form() {
 	) {
 		return;
 	}
-	
+
 	$posted = $_POST;
 	unset( $posted[ 'ttm_form' ] );
 	unset( $posted[ 'post_id' ] );
@@ -214,28 +217,52 @@ function process_form() {
 		$fields = process_inner_blocks( $block[ 'innerBlocks' ] );
 		sort( $fields );
 
-		if(
-			$fields === $keys &&
-			is_array( $block[ 'attrs' ] )
-		) {
+		$fields = array_filter( $fields, function( $val ) {
+			return ! empty( $val );
+		} );
+
+		$insert = true;
+		foreach( $fields as $field ) {
+			if( ! in_array( $field, $keys ) ) {
+				$insert = false;
+			}
+		}
+
+		if( $insert ) {
+			$block[ 'attrs' ] = array_filter( $block[ 'attrs' ], function( $key ) {
+				return in_array( $key, [ 'post_id', 'to', 'subject' ] );
+			}, ARRAY_FILTER_USE_KEY );
 			$attrs = $block[ 'attrs' ];
 			break;
 		}
 	}
 
+	$attrs[ 'to' ] = is_email( $attrs[ 'to' ] );
 	$to = is_email( $attrs[ 'to' ] );
+	$attrs[ 'subject' ] = sanitize_text_field( $attrs[ 'subject' ] );
 	$subject = sanitize_text_field( $attrs[ 'subject' ] );
 
 	$message = '';
 	$headers = [];
+	$fields = $attrs;
 
 	foreach( $_POST as $key => $value ) {
+		$key = sanitize_text_field( $key );
+		$value = sanitize_text_field( $value );
+		$fields[ $key ] = $value;
+
 		if( $key === 'ttm_form' ) {
 			continue;
 		}
-		$value = sanitize_text_field( $value );
 		$message .= " $key: $value";
 	}
+	$fields = json_encode( json_decode( json_encode( $fields ) ) );
+
+	$date = date( 'Y-m-d' );
+	$url = get_the_permalink( $attrs[ 'post_id' ] );
+	
+	insert_record_into_table( $date, $url, $fields );
+
 	wp_mail( $to, $subject, $message, $headers );
 	header("Location: {$_SERVER[ 'REQUEST_URI' ]}");
 	die;
@@ -259,3 +286,44 @@ function register_blocks() {
 	register_block_type( __DIR__ . '/blocks/ttm-textarea/build' );
 }
 add_action( 'init', __NAMESPACE__ . '\register_blocks' );
+
+/**
+ * 
+ */
+function insert_record_into_table( $date, $url, $fields ) {
+
+	global $wpdb;
+
+	$table_name = TTM_FORMS_TABLE_NAME;
+
+	$wpdb->insert(
+		$table_name,
+		[
+			'date' => $date,
+			'url' => $url,
+			'fields' => $fields,
+		]
+	);
+}
+
+/**
+ * 
+ */
+function create_database_table() {
+	global $wpdb;
+
+	$table_name = TTM_FORMS_TABLE_NAME;
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		date date DEFAULT '0000-00-00' NOT NULL,
+		url varchar(55) DEFAULT '' NOT NULL,
+		fields text NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+	dbDelta( $sql );
+}
+register_activation_hook( __FILE__, __NAMESPACE__ . '\create_database_table' );
